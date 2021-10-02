243a658f0a0228b487253d51f3df1dc3b5b47d13
shanedasha <chirkova_99@inbox.ru>
2021-10-02 13:23:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 80;
+        int amountForMile = 20;
+        int freeMile= (ticketPrice/amountForMile);
+        System.out.println(freeMile);
+    }
+}
